d7898c7a93d9b73cb30f9242113d8119d7db8be7
EvanBtc <evan.bitic@gmail.com>
2022-07-20 16:34:08 +0200

    render template example

--- app/app.py
+++ app/app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "<h1>Welcome on your website ! @evanbtc :)</h1>"
+
+# If you want to return html file (from flask import render_template)
+# Your html file needs to be in the template folder (app/templates/index.html) 
+@app.route('/html')
+def html():
+	return render_template('index.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
